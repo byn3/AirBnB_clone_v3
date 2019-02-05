0c9cc6b7703ff858f0b0dcc7ba75e9e4b9c7ef98
anovacap <anovacap@yahoo.com>
2019-02-05 17:25:24 +0000

    add __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
 from models.base_model import BaseModel
+from models.state import State
 app_views = Blueprint('app_views', '__name__', url_prefix='/api/v1')
 from api.v1.views.index import *  # noqa: E402
